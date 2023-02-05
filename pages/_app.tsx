6a7572429fbc69bfa3b5597c7a61853b94969513
shivam <jhodshivam5122@gmail.com>
2023-02-05 05:46:02 +0530

    blog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import React, {useEffect, useState} from 'react'
 import { Layout } from '../components'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <Layout>
 
   <Component {...pageProps} />
+  <Analytics />
   </Layout>
   )
 }
