3b8ceaadd058443273764fbcd4895382dd40b9de
shivam <jhodshivam5122@gmail.com>
2023-02-05 05:33:01 +0530

    blog

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         
         useEffect(() => {
                 getCategories().then((newCategories) => {
-                setCategories(newCategories);
+                setCategories(newCategories)
             });
         }, []);
   return (
